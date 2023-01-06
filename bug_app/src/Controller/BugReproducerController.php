46005d24fab19c72b39fb9e2f68fefabcec72860
pforesi <pierre.foresi@orange.com>
2023-01-06 10:21:24 +0100

    Adding Section 'Steps to check the bug'

--- bug_app/src/Controller/BugReproducerController.php
+++ bug_app/src/Controller/BugReproducerController.php
@@ -14,8 +14,20 @@ class BugReproducerController extends AbstractController
     {
         $response = new StreamedResponse();
         $response->setCallback(function () use ($client) {
-            $response = $client->request('GET', 'https://symfony.com');
-            echo $response->getStatusCode();
+            $url = 'https://symfony.com';
+            $response = $client->request('GET', $url);
+            $statusCode = $response->getStatusCode();
+            echo <<< EOD
+            Call to <b>$url</b> done with status: <b>$statusCode</b>
+            <p>
+            Steps to check the bug: 
+            <ol>
+            <li>Go to <a href="/_profiler/latest?panel=http_client" target="_blank">web profiler's latest token Http Client panel</a></li>
+            <li>Confirm the bug => The panel is empty</li>
+            <li>Go to <a href="/_profiler/latest?panel=logger" target="_blank">web profiler's latest token Logs panel</a></li>
+            <li>We can see that the request has been successfully done</li>
+            </ol>
+            EOD;
         });
 
         return $response;
